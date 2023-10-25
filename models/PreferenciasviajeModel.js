8b5128ea79aad8856e604e8e5a54f3a1090a73de
KarenGilces <you@example.com>
2023-10-24 21:10:20 -0500

    pre

--- models/PreferenciasviajeModel.js
+++ models/PreferenciasviajeModel.js
@@ -24,3 +24,32 @@ export const PreferenciasviajeModel = sequelize.define(
     timestamps: false,
   }  
   );
+  const PreferenciasData = [
+    { preferencia: "Hablo por los codos!" },
+    { preferencia: "Hablo cuando me siento comodo" },
+    { preferencia: "no Hablo mucho" },
+    { preferencia: "Fumar no supone un problema" },
+    { preferencia: "Acepto paradas para fumar" }, 
+    { preferencia: "No quiero que se fume" },
+    { preferencia: "Con la música a todas partes!" }, 
+    { preferencia: "Escucho música dependiendo la situación" },
+    { preferencia: "El silencio es oro" }, 
+    { preferencia: "Me encantan la mascotas. ¡Guau, guau!" },
+    { preferencia: "Viajo con mascotas en función del animal" }, 
+    { preferencia: "Prefiero no viajar con mascotas" }, 
+    
+  ];
+  async function sedPreferencias() {
+    try {
+      const preferencia = await PreferenciasviajeModel.findAll();
+      if(preferencia && preferencia.length <= 0){
+        for (const PreferenciaData of PreferenciasData) {
+          await PreferenciasviajeModel.create(PreferenciaData);
+        }
+      }
+    } catch (error) {
+      console.error("Error al insertar los colores:", error);
+    } 
+  }
+
+  sedPreferencias();
